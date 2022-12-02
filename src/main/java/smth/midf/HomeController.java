513c3ae0679097dafa34ea1032786b052a38e61f
yakorch <korchyar19@gmail.com>
2022-12-02 22:22:36 +0200

    some changes

--- src/main/java/smth/midf/HomeController.java
+++ src/main/java/smth/midf/HomeController.java
@@ -2,12 +2,19 @@ package smth.midf;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HomeController {
     @GetMapping(value = "/")
     public String base() {
         return "index.html";
+    }
+
+    @PostMapping
+    public void addCompany(@RequestBody String url) {
+
     }
 
 }
