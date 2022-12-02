a73f33316ce7237d76820bc5a020ffa955fdaf6a
yakorch <korchyar19@gmail.com>
2022-12-02 22:53:39 +0200

    demo version

--- src/main/java/smth/midf/HomeController.java
+++ src/main/java/smth/midf/HomeController.java
@@ -1,20 +1,32 @@
 package smth.midf;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import smth.midf.parsingdata.CompanyInfo;
+import smth.midf.parsingdata.Parser;
 
+import java.io.IOException;
+
+@RestController
+//@RequestMapping
 @Controller
 public class HomeController {
+    private CompanyInfo companyInfo;
     @GetMapping(value = "/")
     public String base() {
         return "index.html";
     }
 
     @PostMapping
-    public void addCompany(@RequestBody String url) {
-
+    public void addCompany(@RequestBody String url) throws IOException {
+        companyInfo = CompanyInfo.builder().build();
+        Parser.getData(url, companyInfo);
     }
+
+    @GetMapping(path = "/{url}")
+    public CompanyInfo yahoo(@PathVariable String url){
+        return companyInfo;
+    }
+
 
 }
