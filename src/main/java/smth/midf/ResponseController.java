49fc1990d0fc51087f9a54d822b9832ebb6cff11
yakorch <korchyar19@gmail.com>
2022-12-03 03:14:48 +0200

    changes on the server side

--- src/main/java/smth/midf/ResponseController.java
+++ src/main/java/smth/midf/ResponseController.java
@@ -11,8 +11,8 @@ import smth.midf.parsingdata.Parser;
 @RequestMapping(path = "/result", method = RequestMethod.POST)
 public class ResponseController {
 
-    private String first_part = "<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8'><title>DataFinder</title></head><body><h1>DataFinder</h1><form><form id='contact-form' action='/result' method='POST'><input type='text' id='domain' name='domain'><input type='submit' formaction='/result'></form><p id='result'>";
-    private String second_part = "</p></form></body></html>";
+    private final String first_part = "<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8'><title>DataFinder</title></head><body><h1>DataFinder</h1><form><form id='contact-form' action='/result' method='POST'><input type='text' id='domain' name='domain'><input type='submit' formaction='/result'></form><p id='result'>";
+    private final String second_part = "</p></form></body></html>";
 
     @SneakyThrows
     @GetMapping
